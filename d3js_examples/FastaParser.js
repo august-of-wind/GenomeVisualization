3d04f7f46bacf1d243e7c95e9738a1484097c799
august-of-wind <ameyer5@luc.edu>
2015-06-18 12:30:09 -0500

    I have added a testFasta.json file that serves as a testing JSON file - as of now ColorfulLongScroll.html displays testFasta.json's elements correctly. I am now expanding the fasta parser to return the correct JSON format, so that parsed files can be visualized correctly.

--- d3js_examples/FastaParser.js
+++ d3js_examples/FastaParser.js
@@ -4,7 +4,7 @@ function getFasta(fastaFile){
     	var geneTitleArray = [];
     	var geneArray = [];
     	var geneTitleArrayIndex = 0;
-    	console.log("line length:" + lines.length);
+    	//console.log("line length:" + lines.length);
 
     	for(var lineIndex = 0, lineLength = lines.length; lineIndex < lineLength; ++lineIndex){
     		if(lines[lineIndex].indexOf(">") == -1){ //if part of sequence, add nucleotides to geneArray
@@ -21,7 +21,9 @@ function getFasta(fastaFile){
     			geneTitleArrayIndex++;		
     		}
     	}
+
     	//Commented out lines are for testing purpsoses and are not necessary otherwise
+
     	// console.log("GeneTitleArray Contents: " + geneTitleArray); //Make sure the first 11 elements in the geneArray are the correct genes
    		// for(first=0; first < 10; ++first){ 
    		// 	console.log("The nucleotide at" + first + " is: " +geneArray[first+1]);
@@ -35,20 +37,65 @@ function getFasta(fastaFile){
 
 
     	var geneArrayLength = geneArray.length; //declare a constant geneArray length
+      var arrayForJSON = []; //declare array for JSON to be generated from
+      //var myJSON = []; //declare blank JSON
 
-		var fastaDiv = $("#fastaDiv"); //declare constant for the div you'll add your parsed data to
-  		for(var x = 0; x < geneArrayLength; x++) //create a new DOM element for each element of the geneArray for simple visualization purposes
-  		{
-   			if(x>10){ //test that the first ten nucleotides display correctly
-   				console.log("Hi: " + x);
-   				break;
-   			}
+      var items = {
+        "items": [];
+      }
+      var idCount = 0;
+      var contigCount = 0;
+      for(var geneArrayIndex = 0; geneArrayIndex < geneArrayLength - 1; geneArrayIndex++) //go through geneArray creating arrayForJSON
+      {
+        if(geneArray[geneArrayIndex].indexOf(">") > -1 && contigCount == 0){
+          var nucleotide = 
+          {
+          "name": geneArray[geneArrayIndex];
+          "label": geneArray[geneArrayIndex];
+          "id": idCount;
+          }
+          idCount++;
+        }
+        
+        if(geneArray[geneArrayIndex].indexOf(">") > -1)
+        {
+          var name =
+          {
+            "name": geneArray[geneArrayIndex] //add sequence titles to JSON
+          };
+          items.push(name);
+        }
+        else{
+          var label =
+          {
+            "label": geneArray[geneArrayIndex] //add nucleotides to JSON
+            "id": idCount;
+            idCount++;
+          };
+          items.push(sequenceNucleotide);
+        }
+      }
+      // console.log(arrayForJSON[0]);
+      console.log(arrayForJSON[55]);
+      console.log(arrayForJSON.length);
+      // console.log("Donezo");
+      return arrayForJSON;
 
-   			var pElement = document.createElement('p');
-   			console.log(pElement);
-    		pElement.innerHTML = geneArray[x];
-    		fastaDiv.append(pElement	);
-    		pElement = null; //set pElement to null so that it is not visible to the next iteration of the loop.
-    	}
+    //below lines are for displaying the first ten indices of the geneArray for testing purposes and can be disregarded otherwise.
+
+	// var fastaDiv = $("#fastaDiv"); //declare constant for the div you'll add your parsed data to
+  // 		for(var x = 0; x < geneArrayLength; x++) //create a new DOM element for each element of the geneArray for simple visualization purposes
+  // 		{
+  //  			if(x>10){ //test that the first ten nucleotides display correctly
+  //  				console.log("Hi: " + x);
+  //  				break;
+  //  			}
+
+  //  			var pElement = document.createElement('p');
+  //  			console.log(pElement);
+  //   		pElement.innerHTML = geneArray[x];
+  //   		fastaDiv.append(pElement	);
+  //   		pElement = null; //set pElement to null so that it is not visible to the next iteration of the loop.
+  //   	}
     }); 
 }
